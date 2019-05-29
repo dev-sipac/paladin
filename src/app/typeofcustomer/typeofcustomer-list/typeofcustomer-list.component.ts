6921749168bbd6ab54ba8ff44ffae6238c421807
devsipac <cristhian.sipac@mail.com>
2019-05-28 18:31:55 -0600

    Modulo de clientes funcional

--- src/app/typeofcustomer/typeofcustomer-list/typeofcustomer-list.component.ts
+++ src/app/typeofcustomer/typeofcustomer-list/typeofcustomer-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {TypeofcustomerDataModel} from '../typeofcustomer-data.model';
+import {MatDialog} from '@angular/material';
+import {TypeofcustomerService} from '../typeofcustomer.service';
+import {TypeofpartnerNewComponent} from '../../typeofpartner/typeofpartner-new/typeofpartner-new.component';
 
 @Component({
   selector: 'app-typeofcustomer-list',
@@ -6,10 +10,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./typeofcustomer-list.component.css']
 })
 export class TypeofcustomerListComponent implements OnInit {
+  codigo: string;
+  descripcion: string;
 
-  constructor() { }
+  customerdata: TypeofcustomerDataModel[] = [];
+  didFail = false;
+  user: TypeofcustomerDataModel;
+  lowerIsBetter = false;
+  filter = 'all';
+
+  constructor(public dialog: MatDialog, private CustomerService: TypeofcustomerService) { }
 
   ngOnInit() {
+    this.user = this.CustomerService.userData;
+    this.CustomerService.dataEdited.subscribe(
+      () => this.user = this.CustomerService.userData
+    );
+    this.CustomerService.dataLoaded.subscribe(
+      (data: TypeofcustomerDataModel[]) => {
+        this.customerdata = data;
+      }
+    );
+    this.CustomerService.dataLoadFailed.subscribe(
+      (didFail: boolean) => this.didFail = didFail
+    );
+  }
+
+  onFilter(filter: string) {
+    this.filter = filter;
+  }
+
+  onSelectLower(isBetter: boolean) {
+    this.lowerIsBetter = isBetter;
+  }
+
+  getListGroupItemClass(item: TypeofcustomerDataModel) {
+    if (+item[this.filter] === +this.user[this.filter]) {
+      return 'list-group-item-warning';
+    }
+    if (this.lowerIsBetter) {
+      return this.user[this.filter] < item[this.filter] ? 'list-group-item-success' : 'list-group-item-danger';
+    } else {
+      return this.user[this.filter] > item[this.filter] ? 'list-group-item-success' : 'list-group-item-danger';
+    }
+  }
+
+  onGetResults() {
+    console.log('Hey');
+    this.CustomerService.onRetrieveData();
+  }
+
+  onClearData() {
+    this.CustomerService.onDeleteData();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TypeofpartnerNewComponent, {
+      width: '450px',
+      data: {codigo: this.codigo, descripcion: this.descripcion}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.descripcion = result;
+      this.onGetResults();
+    });
   }
 
 }
--- src/app/typeofcustomer/typeofcustomer-new/typeofcustomer-new.component.ts
+++ src/app/typeofcustomer/typeofcustomer-new/typeofcustomer-new.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {TypeofcustomerService} from '../typeofcustomer.service';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {TypeofcustomerDataModel} from '../typeofcustomer-data.model';
 
 @Component({
   selector: 'app-typeofcustomer-new',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./typeofcustomer-new.component.css']
 })
 export class TypeofcustomerNewComponent implements OnInit {
+  codigo: string;
+  descripcion: string;
+  doInput = true;
+  isLoading = false;
+  couldNotLoadData = false;
 
-  constructor() { }
+  constructor(private typeofcustomerService: TypeofcustomerService, public dialogRef: MatDialogRef<TypeofcustomerNewComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: TypeofcustomerDataModel) { }
 
-  ngOnInit() {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
+  ngOnInit() {
+    this.typeofcustomerService.dataIsLoading.subscribe(
+      (isLoading: boolean) => this.isLoading = isLoading
+    );
+    this.typeofcustomerService.dataLoadFailed.subscribe(
+      (didFail: boolean) => {
+        this.couldNotLoadData = didFail;
+        this.isLoading = false;
+      }
+    );
+  }
+
+  onSubmit() {
+    const data: TypeofcustomerDataModel = {
+      codigo: this.codigo as string,
+      descripcion: this.descripcion as string
+    };
+    console.log(data);
+    this.typeofcustomerService.onStoreData(data);
+    this.dialogRef.close();
+  }
 }
